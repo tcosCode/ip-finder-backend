87a083c0009fb583d1590bffad20d7d9291a2a8c
Yordis Monteserin <tcoscode@gmail.com>
2024-12-14 01:16:17 -0800

    minor fixes

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
-import fetch from "node-fetch";
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
 
 const app = express();
 
+// Middleware para habilitar CORS
 app.use(
   cors({
-    origin: "https://geeks-for-geeks-react-projects.vercel.app",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://geeks-for-geeks-react-projects.vercel.app"
+        : "*",
     methods: "GET, POST",
-  })
-);
-
-// Configurar Helmet para CSP
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "https://vercel.live"],
-      },
-    },
   })
 );
 
@@ -28,20 +18,24 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/ip", async (req, res) => {
   const ip = req.query.ip || ""; // Lee la IP de la query string
-  const endpoint = ip
+  const isValidIp = /^(\d{1,3}\.){3}\d{1,3}$/.test(ip); // Validación básica de IP
+
+  const endpoint = isValidIp
     ? `http://ip-api.com/json/${ip}`
     : "http://ip-api.com/json/";
 
   try {
     const response = await fetch(endpoint);
     if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
     }
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Error fetching IP data" });
+    res
+      .status(500)
+      .json({ error: "Error fetching IP data", details: error.message });
   }
 });
 
